3886769f37a74401ceae2b047f276f060ed8051e
lucas <15702827+lucasgleba@users.noreply.github.com>
2025-07-17 17:21:57 +0800

    feat: YuzuUSD

---
+++ src/YuzuUSD.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
+
+/**
+ * @title YuzuUSD
+ */
+contract YuzuUSD is ERC20Burnable, ERC20Permit, Ownable2Step {
+    event MinterUpdated(address indexed oldMinter, address indexed newMinter);
+
+    error CannotRenounceOwnership();
+    error OnlyMinter();
+
+    address public minter;
+
+    constructor(
+        address owner
+    ) ERC20("Yuzu USD", "yzUSD") ERC20Permit("Yuzu USD") Ownable(owner) {}
+
+    function setMinter(address newMinter) external onlyOwner {
+        emit MinterUpdated(minter, newMinter);
+        minter = newMinter;
+    }
+
+    function mint(address to, uint256 amount) external {
+        if (msg.sender != minter) revert OnlyMinter();
+        _mint(to, amount);
+    }
+
+    function renounceOwnership() public view override onlyOwner {
+        revert CannotRenounceOwnership();
+    }
+}
