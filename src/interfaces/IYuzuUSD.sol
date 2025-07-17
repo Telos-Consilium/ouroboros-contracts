ee9bcb6c30c55c2771eb3e7db73d122366ee0db3
lucas <15702827+lucasgleba@users.noreply.github.com>
2025-07-17 21:08:08 +0800

    fix: IYuzuUSD

--- src/interfaces/IYuzuUSD.sol
+++ src/interfaces/IYuzuUSD.sol
@@ -4,11 +4,17 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
-import "@openzeppelin/contracts/access/IAccessControl.sol";
 
-interface IYuzuUSD is IERC20, IERC20Metadata, IERC20Permit, IAccessControl {
-    function mint(address _to, uint256 _amount) external;
-    function burn(uint256 _amount) external;
-    function burnFrom(address account, uint256 amount) external;
+interface IYuzuUSD is IERC20, IERC20Metadata, IERC20Permit {
+    function mint(address to, uint256 value) external;
+    function burn(uint256 value) external;
+    function burnFrom(address account, uint256 value) external;
+
+    function owner() external returns (address);
+    function pendingOwner() external returns (address);
+    function transferOwnership(address newOwner) external;
+    function acceptOwnership() external;
+    function renounceOwnership() external;
+
     function setMinter(address newMinter) external;
 }
