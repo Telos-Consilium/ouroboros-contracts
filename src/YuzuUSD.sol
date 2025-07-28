cc95fd7881936e7365d8884aa9961053afacb697
lucas <15702827+lucasgleba@users.noreply.github.com>
2025-07-28 13:59:02 +0800

    docs(yzusd): doc functions

--- src/YuzuUSD.sol
+++ src/YuzuUSD.sol
@@ -10,22 +10,34 @@ import "./interfaces/IYuzuUSDDefinitions.sol";
 
 /**
  * @title YuzuUSD
+ * @dev ERC20 token mintable by a designated minter.
  */
 contract YuzuUSD is ERC20Burnable, ERC20Permit, Ownable2Step, IYuzuUSDDefinitions {
     address public minter;
 
+    /**
+     * @dev Sets the values for {name} and {symbol}.
+     */
     constructor(string memory name_, string memory symbol_, address owner)
         ERC20(name_, symbol_)
         ERC20Permit(name_)
         Ownable(owner)
     {}
 
+    /**
+     * @dev Sets {minter}.
+     */
     function setMinter(address newMinter) external onlyOwner {
         address oldMinter = minter;
         minter = newMinter;
         emit MinterUpdated(oldMinter, newMinter);
     }
 
+    /**
+     * @dev Mints `amount` tokens to `to`.
+     *
+     * Only callable by the minter.
+     */
     function mint(address to, uint256 amount) external {
         if (_msgSender() != minter) revert OnlyMinter();
         _mint(to, amount);
