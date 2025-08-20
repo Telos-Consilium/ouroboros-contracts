da072b7c689f094498c9914be05b82ca50926d3f
lucas <15702827+lucasgleba@users.noreply.github.com>
2025-08-20 21:59:54 +0800

    refactor(issuer): move definitions

--- src/interfaces/proto/IYuzuDefinitions.sol
+++ src/interfaces/proto/IYuzuDefinitions.sol
@@ -2,11 +2,6 @@
 pragma solidity ^0.8.30;
 
 interface IYuzuDefinitions {
-    error ExceededMaxDeposit(address receiver, uint256 assets, uint256 max);
-    error ExceededMaxMint(address receiver, uint256 token, uint256 max);
-    error ExceededMaxWithdraw(address owner, uint256 assets, uint256 max);
-    error ExceededMaxRedeem(address owner, uint256 token, uint256 max);
-
     event Deposit(address indexed sender, address indexed owner, uint256 assets, uint256 tokens);
     event Withdraw(
         address indexed sender, address indexed receiver, address indexed owner, uint256 assets, uint256 tokens
